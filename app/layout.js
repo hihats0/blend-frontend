6405f2b82231982ecd2db69c774cf13c76c86f57
hihats0 <148712590+hihats0@users.noreply.github.com>
2025-08-16 17:20:36 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import Navbar from '@/components/Navbar'
 import SidebarLeft from '@/components/SidebarLeft'
 import RightRail from '@/components/RightRail'
+import Navbar from '@/components/Navbar' // <-- tekrar ekledik
 
 export const metadata = { title: 'Blend', description: 'Twitter-like UI' }
 
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="tr">
       <body>
-        {/* Desktop 3 kolon grid */}
+        {/* Desktop: 3 kolon; Mobile: sadece orta kolon */}
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 min-h-screen grid grid-cols-12 gap-4">
           {/* Sol menü (md ve üstü) */}
           <aside className="hidden md:block md:col-span-3 lg:col-span-3 xl:col-span-3 py-4">
@@ -22,14 +22,16 @@ export default function RootLayout({ children }) {
             <div className="h-28" />
           </main>
 
-          {/* Sağ trendler */}
+          {/* Sağ panel (lg ve üstü) */}
           <aside className="hidden lg:block lg:col-span-3 xl:col-span-3 py-4">
             <RightRail />
           </aside>
         </div>
 
-        {/* Mobil alt navbar */}
-        <Navbar />
+        {/* 🔽 Sadece mobilde alt bar (Navbar) */}
+        <div className="md:hidden">
+          <Navbar />
+        </div>
       </body>
     </html>
   )
